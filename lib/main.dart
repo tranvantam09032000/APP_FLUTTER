153d1946489f6a81b0ec4e6aeeccbefc7e9df18d
DESKTOP-843VIUE\TRANTAM <tranvantam09032000@gmail.com>
2022-09-01 11:55:03 +0700

    Widgets Wrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:app_flutter/screens/main_screen.dart';
+import 'package:app_flutter/widgets_wrap.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MainScreen());
+  runApp(WidgetsWrap());
 }
 
