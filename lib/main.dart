c583372f52e0307d9690fbbdb0767839ca1d7551
DESKTOP-843VIUE\TRANTAM <tranvantam09032000@gmail.com>
2022-09-04 14:48:05 +0700

    Widgets Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:app_flutter/widgets_expanded.dart';
 import 'package:app_flutter/widgets_scaffold.dart';
 import 'package:app_flutter/widgets_wrap.dart';
+import 'package:app_flutter/widgets_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(WidgetsScaffold());
+  runApp(WidgetsContainer());
 }
 
