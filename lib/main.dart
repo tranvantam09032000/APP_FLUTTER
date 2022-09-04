49762d7e46962a47b9469d8c9aa0ec77755742ac
DESKTOP-843VIUE\TRANTAM <tranvantam09032000@gmail.com>
2022-09-04 14:55:37 +0700

    Widgets row and column

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:app_flutter/widgets_column.dart';
 import 'package:app_flutter/widgets_expanded.dart';
+import 'package:app_flutter/widgets_row.dart';
 import 'package:app_flutter/widgets_scaffold.dart';
 import 'package:app_flutter/widgets_wrap.dart';
 import 'package:app_flutter/widgets_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(WidgetsContainer());
+  runApp(WidgetsColumn());
 }
 
