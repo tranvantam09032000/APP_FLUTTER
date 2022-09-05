beb44b6b13d9f1968cf05a6c540f8673d2cad9d1
Tâm Trần Văn <tranvantam09032000@gmail.com>
2022-09-05 14:43:34 +0700

    wedgets forms

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app_flutter/widgets_buttons.dart';
 import 'package:app_flutter/widgets_column.dart';
 import 'package:app_flutter/widgets_expanded.dart';
+import 'package:app_flutter/widgets_forms.dart';
 import 'package:app_flutter/widgets_rich_text.dart';
 import 'package:app_flutter/widgets_row.dart';
 import 'package:app_flutter/widgets_scaffold.dart';
@@ -12,6 +13,6 @@ import 'package:app_flutter/widgets_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(WidgetsStack());
+  runApp(WidgetsForms());
 }
 
